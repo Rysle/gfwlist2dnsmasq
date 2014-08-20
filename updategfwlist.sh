ff3b4e3a9a0167f02698692a85a0fb538738f57a
Rysle <Rysle@qq.com>
2014-08-21 00:35:13 +0800

    enhance log output in updategfwlist.sh

--- updategfwlist.sh
+++ updategfwlist.sh
@@ -1,6 +1,13 @@
-logger "===== Update GFWList Script Begin ====="
-url_dnsmasq_conf_encoded="http://lab.linruizhao.com/list/?get=1&clientmd5="
-url_dnsmasq_conf_md5="http://lab.linruizhao.com/list/?md5=1"
+debug() {
+    echo "$1"
+    logger "$1"
+}
+
+debug "===== Update GFWList Script Begin ====="
+
+url_host_root="http://yourdomain.com/yourpath/"
+url_dnsmasq_conf_encoded="$url_host_root?get=1"
+url_dnsmasq_conf_md5="$url_host_root?md5=1"
 file_dnsmasq_conf_encoded="/tmp/dnsmasq.conf.encoded"
 file_dnsmasq_conf="/tmp/dnsmasq.conf"
 file_dnsmasq_conf_md5="/tmp/dnsmasq.conf.md5"
@@ -10,49 +17,46 @@ flag_ok="/tmp/dnsmasq.update.OK"
 flag_failed="/tmp/dnsmasq.update.FAILED"
 
 md5_current=$(md5sum $target_dnsmasq_conf | awk '{print $1}')
-echo "== current md5: $md5_current"
+debug "== current md5: $md5_current"
+param_clientmd5="&clientmd5=$md5_current"
 
 if [ "$1" == "1" ]; then
     param_force="&force=1"
-    echo "== force update"
-    logger "== force update"
+    debug "== force update"
 else
     param_force=""
-    echo "== normal update"
-    logger "== normal update"
+    debug "== normal update"
 fi
-wget $url_dnsmasq_conf_encoded$md5_current$param_force -O $file_dnsmasq_conf_encoded
-wget $url_dnsmasq_conf_md5 -O $file_dnsmasq_conf_md5
+
+wget "$url_dnsmasq_conf_encoded$param_clientmd5$param_force" -O "$file_dnsmasq_conf_encoded"
+wget "$url_dnsmasq_conf_md5" -O "$file_dnsmasq_conf_md5"
 base64 -d $file_dnsmasq_conf_encoded > $file_dnsmasq_conf
 
-md5_remote=$(cat $file_dnsmasq_conf_md5 | awk '{print $1}')
+md5_remote=$(awk '{print $1}' $file_dnsmasq_conf_md5)
 md5_local=$(md5sum $file_dnsmasq_conf | awk '{print $1}')
-echo "== remote md5: $md5_remote"
-echo "== local md5: $md5_local"
+debug "== remote md5: $md5_remote"
+debug "== local md5: $md5_local"
 
 if [ "$md5_local" == "$md5_remote" ]; then
-    echo "== md5 checked: OK!"
-    logger "== md5 checked: OK!"
+    debug "== md5 checked: OK!"
     if [ -e $flag_failed ]; then
         rm $flag_failed
     fi
     touch $flag_ok
     if [ "$md5_local" != "$md5_current" ]; then
-        echo "== has update! restart dnsmasq..."
-        logger "== has update! restart dnsmasq..."
+        debug "== has update! restart dnsmasq..."
         mv $file_dnsmasq_conf $target_dnsmasq_conf
         /etc/init.d/dnsmasq restart
     else
-        echo "== has no update. wait for next time..."
-        logger "== has no update. wait for next time..."
+        debug "== has no update. wait for next time..."
     fi
 
 else
-    echo "== md5 checked: FAILED!"
-    logger "== md5 checked: FAILED!"
+    debug "== md5 checked: FAILED!"
     if [ -e $flag_ok ]; then
         rm $flag_ok
     fi
     touch $flag_failed
 fi
-logger "===== Update GFWList Script End ====="
+
+debug "===== Update GFWList Script End ====="
